7a14aafff14e89720a5583ffa2e064f6f047a803
0Ascendant0 <tmufakwadziya3@gmail.com>
2026-02-17 07:10:26 +0200

    static fix 2

--- LED_Travel_and_Tours/settings.py
+++ LED_Travel_and_Tours/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'assets'),
+    os.path.join(BASE_DIR, 'frontend', 'static'),
 ]
 STATIC_URL = 'static/'
 
